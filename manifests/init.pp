24003bfafdab02fb327874492819331856cdd2a4
Laurent Domb <github@domb.net>
2016-06-28 06:58:19 -0700

    fixed indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,40 +42,38 @@ class haproxywp (
 
 ) {
 
- $gce = { item => 'fact_values',
-  search => "(name = gce_public_ipv4 or name = gce_public_hostname) and host !~ ${hostname}",
-  per_page => '20',
-  foreman_url  => $foreman_url,
-  foreman_user => $foreman_user,
-  foreman_pass => $foreman_pass }
+  $gce = { item  => 'fact_values',
+    search       => "(name = gce_public_ipv4 or name = gce_public_hostname) and host !~ ${hostname}",
+    per_page     => '20',
+    foreman_url  => $foreman_url,
+    foreman_user => $foreman_user,
+    foreman_pass => $foreman_pass }
 
   $rhev = { item => 'fact_values',
-  search => '(name = rhev_public_ipv4 or name = rhev_public_hostname)',
-  per_page => '20',
-  foreman_url  => $foreman_url,
-  foreman_user => $foreman_user,
-  foreman_pass => $foreman_pass }
+    search       => '(name = rhev_public_ipv4 or name = rhev_public_hostname)',
+    per_page     => '20',
+    foreman_url  => $foreman_url,
+    foreman_user => $foreman_user,
+    foreman_pass => $foreman_pass }
 
+  $ec2 = { item  => 'fact_values',
+    search       => '(name = ec2_public_ipv4 or name = ec2_public_hostname) and host ~ %\.ec2\.internal',
+    per_page     => '20',
+    foreman_url  => $foreman_url,
+    foreman_user => $foreman_user,
+    foreman_pass => $foreman_pass }
 
-  $ec2 = { item => 'fact_values',
-  search => '(name = ec2_public_ipv4 or name = ec2_public_hostname) and host ~ %\.ec2\.internal',
-  per_page => '20',
-  foreman_url  => $foreman_url,
-  foreman_user => $foreman_user,
-  foreman_pass => $foreman_pass }
-
-  $gcehosts = foreman($gce)
-  $ec2hosts = foreman($ec2)
+  $gcehosts  = foreman($gce)
+  $ec2hosts  = foreman($ec2)
   $rhevhosts = foreman($rhev)
-
 
   file {'/etc/haproxy/haproxy.cfg':
         content => template('haproxywp/haproxy.cfg.erb'),
-        owner  => 'root',
-        group  => 'root',
-        mode   => 0644,
+        owner   => 'root',
+        group   => 'root',
+        mode    => 0644,
         require => Package['haproxy'],
-        notify => Service['haproxy'];
+        notify  => Service['haproxy'];
   }
 
   package {'haproxy':
@@ -83,9 +81,8 @@ class haproxywp (
   }
 
   service { 'haproxy':
-      ensure => running,
+      ensure  => running,
       enable  => true,
       require => Package['haproxy'];
   }
-
 }
