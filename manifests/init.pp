a2f85db7beb617019be18b18444adf9244171af8
Laurent Domb <github@domb.net>
2016-06-28 07:01:12 -0700

    removed some doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,12 @@
 # == Class: haproxywp
 #
-# Full description of class haproxywp here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { haproxywp:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Name <laurent@redhat.com>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Laurent Domb
 #
 class haproxywp (
     $foreman_url   = "",
